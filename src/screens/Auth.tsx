26901af019a66e4f6e0a05026358873cd3c0e6e0
gmemmy <gmemmy22@gmail.com>
2020-10-08 16:22:10 +0100

    chore: setup husky and prettier

---
+++ src/screens/Auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Auth = () => {
+  return (
+    <View>
+      <Text>Welcome and hello there!</Text>
+    </View>
+  )
+}
+
+export default Auth
