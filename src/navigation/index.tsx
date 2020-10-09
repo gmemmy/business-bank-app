9ba229d623ec547ba38b4e7a680b2fba10c43f97
gmemmy <gmemmy22@gmail.com>
2020-10-09 04:27:35 +0100

    chore: design navigation tabs

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -9,6 +10,18 @@ import Login from '../screens/Authentication/Login'
 
 // Bottom Tab Screens
 import HomeScreen from './home'
+import { getHeight, getWidth } from '../utils/styles'
+
+// Tab Icons
+const homeActive = require('../../assets/icons/tab/home-active.png')
+const homeInactive = require('../../assets/icons/tab/home-inactive.png')
+const expensesActive = require('../../assets/icons/tab/expenses-active.png')
+const expensesInactive = require('../../assets/icons/tab/expenses-inactive.png')
+const walletActive = require('../../assets/icons/tab/wallet-active.png')
+const walletInactive = require('../../assets/icons/tab/wallet-inactive.png')
+const profileActive = require('../../assets/icons/tab/profile-active.png')
+const profileInactive = require('../../assets/icons/tab/profile-inactive.png')
+const plus = require('../../assets/icons/tab/plus.png')
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -27,20 +40,83 @@ export const Navigator = () => {
 export const TabNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBarOptions={{ showLabel: false }}>
+      <Tab.Navigator tabBarOptions={{ showLabel: true }}>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          // options={{
-          //   tabBarIcon: ({ focused }) =>
-          //     focused ? (
-          //       <Image source={activeHomeIcon} style={styles.icon} />
-          //     ) : (
-          //       <Image source={homeIcon} style={styles.icon} />
-          //     ),
-          // }}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Image source={homeActive} style={styles.icon} />
+              ) : (
+                <Image source={homeInactive} style={styles.icon} />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Expenses"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Image source={expensesActive} style={styles.icon} />
+              ) : (
+                <Image source={expensesInactive} style={styles.icon} />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Plus"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Image source={plus} style={styles.plus} />
+              ) : (
+                <Image source={plus} style={styles.plus} />
+              ),
+            tabBarLabel: '',
+          }}
+        />
+        <Tab.Screen
+          name="Wallet"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Image source={walletActive} style={styles.icon} />
+              ) : (
+                <Image source={walletInactive} style={styles.icon} />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Image source={profileActive} style={styles.icon} />
+              ) : (
+                <Image source={profileInactive} style={styles.icon} />
+              ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    width: getWidth(27),
+    height: getHeight(20),
+    resizeMode: 'contain',
+  },
+  plus: {
+    width: getWidth(48),
+    height: getHeight(48),
+    resizeMode: 'contain',
+    marginBottom: getHeight(10),
+  },
+})
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Welcome to the Home Screen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 export default Home
